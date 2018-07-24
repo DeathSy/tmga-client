ae19e3e7e295d717957658536ae6c4e72a4f737a
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-07-24 16:02:39 +0700

    [ADD] auth client in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Admin, Resource } from 'react-admin'
-import loopbackRestClient from 'aor-loopback'
+import loopbackRestClient, { authClient } from 'aor-loopback'
 
 const dataProvider = loopbackRestClient(process.env.REACT_APP_API_ENDPOINT)
+const authProvider = authClient(process.env.REACT_APP_AUTH_CLIENT)
 
 class App extends Component {
   render () {
     return (
-      <Admin dataProvider={dataProvider} >
+      <Admin
+        dataProvider={dataProvider} >
         <Resource />
       </Admin>
     )
