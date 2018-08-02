88dfbb40a491df0fd1efab12d72249b7025d096e
miiwn <miiwnpmawt@gmail.com>
2018-08-02 09:34:56 +0700

    [EDIT] create form - room.js

--- src/Rooms.js
+++ src/Rooms.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { List, Datagrid, Edit, Create, SimpleForm, DateField, TextField, EditButton, DisabledInput, TextInput, LongTextInput, DateInput } from 'react-admin';
-import BookIcon from '@material-ui/icons/Room';
-export const RoomIcon = BookIcon;
+import React from 'react'
+import { List, Datagrid, Edit, Create, SimpleForm, DateField, TextField, EditButton, TextInput, LongTextInput, DateInput } from 'react-admin'
+import BookIcon from '@material-ui/icons/Room'
+export const RoomIcon = BookIcon
 
 export const RoomList = (props) => (
   <List {...props}>
     <Datagrid>
-      <TextField source="building" />
-      <TextField source="name" />
-      <EditButton basePath="/rooms" />
+      <TextField source='building' />
+      <TextField source='name' />
+      <EditButton basePath='/rooms' />
     </Datagrid>
   </List>
-);
+)
 
 const RoomTitle = ({ record }) => {
-    return <span>Post {record ? `"${record.title}"` : ''}</span>;
-};
+  return <span>Room {record ? `'${record.title}'` : ''}</span>
+}
 
 export const RoomEdit = (props) => (
-    <Edit title={<RoomTitle />} {...props}>
-        <SimpleForm>
-        <TextField source="building" />
-      <TextField source="name" />
-        </SimpleForm>
-    </Edit>
-);
+  <Edit title={<RoomTitle />} {...props}>
+    <SimpleForm>
+      <TextInput source='building' />
+      <TextInput source='name' />
+    </SimpleForm>
+  </Edit>
+)
 
 export const RoomCreate = (props) => (
-    <Create title="Create a Post" {...props}>
-        <SimpleForm>
-        <TextField source="building" />
-      <TextField source="name" />
-        </SimpleForm>
-    </Create>
-);
+  <Create title='Create a room' {...props}>
+    <SimpleForm>
+      <TextInput source='building' />
+      <TextInput source='name' />
+    </SimpleForm>
+  </Create>
+)
