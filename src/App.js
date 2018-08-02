4dbef7c4b97ae013089f587f39fd11942018d355
miiwn <miiwnpmawt@gmail.com>
2018-08-02 09:40:01 +0700

    [EDIT] App.js add lecturer resource

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Admin, Resource } from 'react-admin'
 import loopbackRestClient, { authClient } from 'aor-loopback'
 import { RoomList, RoomEdit, RoomCreate, RoomIcon } from './Rooms.js'
+import { LecturerList, LecturerEdit, LecturerCreate, LecturerIcon } from './Lecturers.js'
 const dataProvider = loopbackRestClient(process.env.REACT_APP_API_ENDPOINT)
 const authProvider = authClient(process.env.REACT_APP_AUTH_CLIENT)
 
@@ -9,8 +10,9 @@ class App extends Component {
   render () {
     return (
       <Admin
-        dataProvider={dataProvider} authProvider={authProvider}>
+        dataProvider={dataProvider} >
         <Resource name='rooms' list={RoomList} edit={RoomEdit} create={RoomCreate} icon={RoomIcon} />
+        <Resource name='lecturers' list={LecturerList} edit={LecturerEdit} create={LecturerCreate} icon={LecturerIcon} />
       </Admin>
     )
   }
