fa0f703adddaf25e23ed8e233f7254020b0a8c7c
miiwn <miiwnpmawt@gmail.com>
2018-09-04 08:40:03 +0700

    [ADD] theme.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import loopbackRestClient, { authClient } from 'aor-loopback'
 import { RoomList, RoomEdit, RoomCreate, RoomIcon } from './Rooms.js'
 import { LecturerList, LecturerEdit, LecturerCreate, LecturerIcon } from './Lecturers.js'
 import { SubjectList, SubjectEdit, SubjectCreate, SubjectIcon } from './Subjects.js'
+import { tmgaTheme } from './theme.js';
+
 const dataProvider = loopbackRestClient(process.env.REACT_APP_API_ENDPOINT)
 const authProvider = authClient(process.env.REACT_APP_AUTH_CLIENT)
 
 class App extends Component {
   render () {
     return (
-      <Admin
+      <Admin theme={tmgaTheme}
+        title={'TMGA '} 
         dataProvider={dataProvider} >
         <Resource name='rooms' list={RoomList} edit={RoomEdit} create={RoomCreate} icon={RoomIcon} />
         <Resource name='lecturers' list={LecturerList} edit={LecturerEdit} create={LecturerCreate} icon={LecturerIcon} />
---
+++ src/theme.js
@@ -0,0 +1,37 @@
+import {
+  createMuiTheme
+} from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+import red from '@material-ui/core/colors/red';
+
+export const tmgaTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1565c0',
+    },
+    secondary: {
+      main: '#0d47a1',
+    },
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+  },
+  overrides: {
+    MuiButton: { // override the styles of all instances of this component
+      root: { // Name of the rule
+        color: 'white', // Some CSS
+      },
+    },
+  },
+});
