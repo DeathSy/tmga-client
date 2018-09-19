990ff0969e54073eb85c7d81200574c3772f19c8
Miiwn <miiwnpmawt@gmail.com>
2018-09-19 17:09:01 +0700

    [EDIT] subject ref field

--- src/Subjects.js
+++ src/Subjects.js
@@ -1,5 +1,28 @@
 import React from 'react'
-import { List, Datagrid, Edit, Create, SimpleForm, NumberField, TextField, EditButton, TextInput, NumberInput, BooleanInput, SelectArrayInput, ArrayField } from 'react-admin'
+import {
+  List,
+  Datagrid,
+  Edit,
+  Create,
+  SimpleForm,
+  NumberField,
+  TextField,
+  EditButton,
+  TextInput,
+  NumberInput,
+  BooleanInput,
+  SelectArrayInput,
+  ArrayField,
+  SingleFieldList,
+  ChipField,
+  ArrayInput,
+  BooleanField,
+  ReferenceField,
+  SelectArrayField,
+  ReferenceManyField,
+  ReferenceArrayField
+} from 'react-admin'
+import Chip from '@material-ui/core/Chip'
 import ClassIcon from '@material-ui/icons/Class'
 export const SubjectIcon = ClassIcon
 
@@ -7,28 +30,37 @@ export const SubjectIcon = ClassIcon
 //   { id: 'Lecture', name: 'Lecture' },
 //   { id: 'Lab', name: 'Lab' }
 // ];
+const TagsField = ({ record }) =>
+  record.students.map((year, index) => (
+    <Chip style={{ margin: 4 }} key={index} label={year} />
+  ))
+
 const years = [
-  { id: '1', name: '1st year' },
-  { id: '2', name: '2nd year' },
-  { id: '3', name: '3rd year' },
-  { id: '4', name: '4th year' }
-];
+  { id: '1st year', year: '1st year' },
+  { id: '2nd year', year: '2nd year' },
+  { id: '3rd year', year: '3rd year' },
+  { id: '4th year', year: '4th year' }
+]
 
 export const SubjectList = (props) => (
   <List {...props}>
     <Datagrid>
       <TextField source='code' label='Subject Code' />
       <TextField source='name' label='Subject Name' />
-      <ArrayField source='year' />
-      <NumberField source='sectionAmount' label='Section Amount' />
-      <NumberField source='studentsPerSection' label='Students per section' />
+      <ReferenceArrayField label='Subject Type' reference='subjectFormatsSubject' source='subjectId' >
+        <SingleFieldList>
+          <TextField source='name' />
+        </SingleFieldList>
+      </ReferenceArrayField>
+      <TagsField label='Student Year' />
+      <BooleanField source='isRequired' label='Required' />
       <EditButton basePath='/subjects' />
     </Datagrid>
   </List>
 )
 
 const SubjectTitle = ({ record }) => {
-  return <span>Subject {record ? `'${record.title}'` : ''}</span>
+  return <span>Subject {record ? `'${record.name}'` : ''}</span>
 }
 
 export const SubjectEdit = (props) => (
@@ -51,8 +83,11 @@ export const SubjectCreate = (props) => (
       <TextInput source='name' />
       <NumberInput source='sectionAmount' />
       <NumberInput source='studentsPerSection' />
-      <SelectArrayInput source='year' choices={years} />
-      <BooleanInput label='Compulsory' source='isCompulsory' />
+      <SelectArrayInput source='students' choices={years} optionText='year' />
+      <ReferenceField label='Subject Type' source='subjectFormatId' reference='subjectFormats'>
+        <SelectArrayField />
+      </ReferenceField>
+      <BooleanInput label='Required' source='isRequired' />
     </SimpleForm>
   </Create>
 )
