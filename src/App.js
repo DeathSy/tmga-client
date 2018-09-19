c18bd9e499ee97efec4bf74fdf7e472f1ad6e709
Miiwn <miiwnpmawt@gmail.com>
2018-09-19 10:13:09 +0700

    [EDIT] App add theme and title

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import { RoomList, RoomEdit, RoomCreate, RoomIcon } from './Rooms.js'
 import { LecturerList, LecturerEdit, LecturerCreate, LecturerIcon } from './Lecturers.js'
 import { SubjectList, SubjectEdit, SubjectCreate, SubjectIcon } from './Subjects.js'
 import Dashboard from './Dashboard'
-
-
+// import MyLayout from './MyLayout'
+import tmgaTheme from './theme.js'
+//"http://api.tmga.cf/api/"
 const dataProvider = loopbackRestClient(process.env.REACT_APP_API_ENDPOINT)
 const authProvider = authClient(process.env.REACT_APP_AUTH_CLIENT)
 
 class App extends Component {
+  
   render () {
     return (
       <Admin dashboard={Dashboard} 
+        theme={tmgaTheme}
+        title={'TMGA'}
         dataProvider={dataProvider} >
         <Resource name='rooms' list={RoomList} edit={RoomEdit} create={RoomCreate} icon={RoomIcon} />
         <Resource name='lecturers' list={LecturerList} edit={LecturerEdit} create={LecturerCreate} icon={LecturerIcon} />
         <Resource name='subjects' list={SubjectList} edit={SubjectEdit} create={SubjectCreate} icon={SubjectIcon} />
+        <Resource name='subjectformats' />
       </Admin>
     )
   }
