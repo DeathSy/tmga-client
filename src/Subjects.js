dc5c6375da51255b55743b6fefb23c2c8986ce97
Miiwn <miiwnpmawt@gmail.com>
2018-09-19 10:10:59 +0700

    [EDIT] subject add year

--- src/Subjects.js
+++ src/Subjects.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { List, Datagrid, Edit, Create, SimpleForm, NumberField, TextField, EditButton, TextInput, NumberInput, BooleanInput, ArrayField, SingleFieldList, ChipField } from 'react-admin'
+import { List, Datagrid, Edit, Create, SimpleForm, NumberField, TextField, EditButton, TextInput, NumberInput, BooleanInput, SelectArrayInput, ArrayField } from 'react-admin'
 import ClassIcon from '@material-ui/icons/Class'
 export const SubjectIcon = ClassIcon
+
+// const choices = [
+//   { id: 'Lecture', name: 'Lecture' },
+//   { id: 'Lab', name: 'Lab' }
+// ];
+const years = [
+  { id: '1', name: '1st year' },
+  { id: '2', name: '2nd year' },
+  { id: '3', name: '3rd year' },
+  { id: '4', name: '4th year' }
+];
 
 export const SubjectList = (props) => (
   <List {...props}>
     <Datagrid>
       <TextField source='code' label='Subject Code' />
-      <TextField source='name' label='Subject  Name' />
-      <ArrayField source='id'>
-        <SingleFieldList>
-          <ChipField source='formats' label='Subject Type' />
-        </SingleFieldList>
-      </ArrayField>
+      <TextField source='name' label='Subject Name' />
+      <ArrayField source='year' />
       <NumberField source='sectionAmount' label='Section Amount' />
       <NumberField source='studentsPerSection' label='Students per section' />
       <EditButton basePath='/subjects' />
@@ -31,6 +38,7 @@ export const SubjectEdit = (props) => (
       <TextInput source='code' />
       <NumberInput source='sectionAmount' />
       <NumberInput source='studentsPerSection' />
+      <SelectArrayInput source='year' choices={years} />
       <BooleanInput label='Compulsory' source='isCompulsory' />
     </SimpleForm>
   </Edit>
@@ -43,6 +51,7 @@ export const SubjectCreate = (props) => (
       <TextInput source='name' />
       <NumberInput source='sectionAmount' />
       <NumberInput source='studentsPerSection' />
+      <SelectArrayInput source='year' choices={years} />
       <BooleanInput label='Compulsory' source='isCompulsory' />
     </SimpleForm>
   </Create>
