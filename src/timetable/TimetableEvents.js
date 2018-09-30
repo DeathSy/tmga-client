27b8087a40c7b9075358b3db5c5e99cba82913a1
Miiwn <miiwnpmawt@gmail.com>
2018-09-30 15:54:47 +0700

    [EDIT] font timetable

--- src/timetable/TimetableEvents.js
+++ src/timetable/TimetableEvents.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 import moment from 'moment';
+import { GET_LIST, GET_MANY, Responsive, Title } from 'react-admin';
+import loopbackRestClient from 'aor-loopback'
 import axios from 'axios'
+import { withStyles } from '@material-ui/core/styles';
 
 import TimeTable from 'react-timetable-events'
+import { Table, TableHead, TableBody, TableCell, TableRow } from '@material-ui/core';
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+const dataProvider = loopbackRestClient(process.env.REACT_APP_API_ENDPOINT)
 
-export default class TimetableEvent extends Component {
+class TimetableEvent extends Component {
 
-  state = {
+  state= {
     data: {},
     events: {
       MON: [],
@@ -15,7 +30,10 @@ export default class TimetableEvent extends Component {
       THU: [],
       FRI: []
     },
+    timelist: [],
+    eventlist: [],
   }
+
 
   componentWillMount = async () => {
     const { data } = await axios.get('http://ml.tmga.cf/timetables/2/2018')
@@ -62,7 +80,28 @@ export default class TimetableEvent extends Component {
         })
       });
       this.setState({ events: sectionDay })
+      console.log('event', this.state.events)
     }
+  }
+
+  componentDidMount(){
+
+      // dataProvider(GET_LIST,'TimeSlots', {
+      //   pagination: { page: 1, perPage: 20 },
+      //   sort: { field: 'start', order: 'ASC' },
+      // }).then(response => response.data)
+      // .then(slots => {
+      //   const timeslot = slots.map((slot) =>(
+      //     <TableCell>{slot.start+' - '+slot.end}</TableCell>
+      //   )
+      //   )
+      //   this.setState({timelist: timeslot});
+      //   console.log('events',this.state.timelist)
+      // });
+      
+
+
+     
   }
 
   renderHour(hour, defaultAttributes, styles) {
@@ -79,15 +118,15 @@ export default class TimetableEvent extends Component {
       <div {...defaultAttributes}
            title={event.name}
            key={event.id}
-           
+
         >
-        <span className={styles.event_info} style={{ fontWeight: 'bold' }}>
+        <span className={styles.event_info} style={{ fontWeight: 'bold',fontSize: 15 ,color: "#4a4f56", fontFamily: 'Roboto, serif', fontWeight: 'bold' }}>
            { event.name } 
         </span>
-        <span className={styles.event_info} style={{ fontSize: 12 }}>
+        <span className={styles.event_info} style={{ fontSize: 12, color: "#4a4f56", fontFamily: 'Roboto, serif', }}>
            { event.room } 
         </span>
-        <span className={styles.event_info} style={{ fontSize: 12 }}>
+        <span className={styles.event_info} style={{ fontSize: 12 , color: "#4a4f56", fontFamily: 'Roboto, serif',}}>
           { event.startTime.format('HH:mm') } - { event.endTime.format('HH:mm') }
         </span>
       </div>
@@ -95,14 +134,37 @@ export default class TimetableEvent extends Component {
   }
 
   render () {
+    const { classes } = this.props;
+    const { MON } = this.state.events
     return (
-      <TimeTable
-        events={this.state.events}
-        renderHour={this.renderHour}
-        renderEvent={this.renderEvent}
-        hoursInterval={[ 8, 21 ]}
-        timeLabel="Time :)"
-      />
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            {this.state.timelist}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+         
+          {Object.keys(MON).map(day => 
+          Object.keys(MON[day]).map(room => MON[day][room].map((section, index) => {
+            if (index === 0) {
+              return <TableCell colspan={section.slot} style={{ textAlign: 'center'}}>{section.name}</TableCell>
+            }
+            return (
+                <TableCell colspan={section.slot} style={{ textAlign: 'center'}}>{section.name}</TableCell>
+            )}
+          )))}
+        </TableBody>
+      </Table>
+      // <TimeTable
+      //   events={this.state.events}
+      //   renderHour={this.renderHour}
+      //   renderEvent={this.renderEvent}
+      //   hoursInterval={[ 8, 21 ]}
+      //   timeLabel="Time :)"
+      // />
     )
   }
 }
+export default withStyles(styles)(TimetableEvent)
