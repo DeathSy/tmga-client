95ad3b25fca8b4ce717fc7e491ddc1f1c30b0c33
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-11-14 15:20:08 +0700

    [EDIT] code formatting in app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react'
 import { Admin, Resource } from 'react-admin'
 import loopbackRestClient, { authClient } from 'aor-loopback'
 import { RoomList, RoomEdit, RoomCreate, RoomIcon } from './Rooms.js'
-import { LecturerList, LecturerEdit, LecturerCreate, LecturerIcon } from './Lecturers.js'
-import { SubjectList, SubjectEdit, SubjectCreate, SubjectIcon } from './Subjects.js'
-import { FixedSubjectList, FixedSubjectEdit, FixedSubjectCreate, FixedSubjectIcon } from './FixedSubjects.js'
-import { ConstrainList, ConstrainEdit, ConstrainCreate, ConstrainIcon } from './Constrains.js'
-import { SectionList, SectionEdit, SectionCreate, SectionIcon } from './Sections.js'
-import { SubjectFormatList, SubjectFormatEdit, SubjectFormatCreate, SubjectFormatIcon } from './SubjectFormats.js'
+import {
+  LecturerList,
+  LecturerEdit,
+  LecturerCreate,
+  LecturerIcon
+} from './Lecturers.js'
+import {
+  SubjectList,
+  SubjectEdit,
+  SubjectCreate,
+  SubjectIcon
+} from './Subjects.js'
+import {
+  SubjectFormatList,
+  SubjectFormatEdit,
+  SubjectFormatCreate,
+  SubjectFormatIcon
+} from './SubjectFormats.js'
 import { TimetableList, TimetableShow, TimetableIcon } from './Timetable'
-// import { CreateView, CreateIcon } from './CreateTimetable'
+import Create from './create/index'
 import Dashboard from './dashboard/Dashboard'
 import tmgaTheme from './theme.js'
 const dataProvider = loopbackRestClient(process.env.REACT_APP_API_ENDPOINT)
@@ -18,22 +30,57 @@ const dataProvider = loopbackRestClient(process.env.REACT_APP_API_ENDPOINT)
 class App extends Component {
   render () {
     return (
-      <Admin dashboard={Dashboard}
+      <Admin
+        dashboard={Dashboard}
         theme={tmgaTheme}
         title={'TMGA'}
-        dataProvider={dataProvider} >
+        dataProvider={dataProvider}
+      >
         {/* <Resource name='subjectSections' options={{ label: 'Create timetable' }} list={SectionList} edit={SectionEdit} create={SectionCreate} icon={SectionIcon} /> */}
+        <Resource name='create' list={Create} />
         <Resource name='timetablesview' />
-        <Resource name='timetables' options={{ label: 'Timetable' }} show={TimetableShow} list={TimetableList} icon={TimetableIcon} />
-        <Resource name='rooms' list={RoomList} edit={RoomEdit} create={RoomCreate} icon={RoomIcon} />
-        <Resource name='lecturers' list={LecturerList} edit={LecturerEdit} create={LecturerCreate} icon={LecturerIcon} />
-        <Resource name='subjects' list={SubjectList} edit={SubjectEdit} create={SubjectCreate} icon={SubjectIcon} />
+        <Resource
+          name='timetables'
+          options={{ label: 'Timetable' }}
+          show={TimetableShow}
+          list={TimetableList}
+          icon={TimetableIcon}
+        />
+        <Resource
+          name='rooms'
+          list={RoomList}
+          edit={RoomEdit}
+          create={RoomCreate}
+          icon={RoomIcon}
+        />
+        <Resource
+          name='lecturers'
+          list={LecturerList}
+          edit={LecturerEdit}
+          create={LecturerCreate}
+          icon={LecturerIcon}
+        />
+        <Resource
+          name='subjects'
+          list={SubjectList}
+          edit={SubjectEdit}
+          create={SubjectCreate}
+          icon={SubjectIcon}
+        />
         {/* <Resource name='fixedSubjects' options={{ label: 'GEN & LNG' }} list={FixedSubjectList} edit={FixedSubjectEdit} create={FixedSubjectCreate} icon={FixedSubjectIcon} />
         <Resource name='constrains' options={{ label: 'Constraints' }} list={ConstrainList} edit={ConstrainEdit} create={ConstrainCreate} icon={ConstrainIcon} /> */}
-        <Resource name='subjectFormats' options={{ label: 'Subject Type' }} list={SubjectFormatList} edit={SubjectFormatEdit} create={SubjectFormatCreate} icon={SubjectFormatIcon} />
+        <Resource
+          name='subjectFormats'
+          options={{ label: 'Subject Type' }}
+          list={SubjectFormatList}
+          edit={SubjectFormatEdit}
+          create={SubjectFormatCreate}
+          icon={SubjectFormatIcon}
+        />
         <Resource name='timeSlots' />
       </Admin>
     )
   }
 }
+
 export default App
