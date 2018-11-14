8d70a2b4eec742bf15b108634b8acad59b32199a
Miiwn <miiwnpmawt@gmail.com>
2018-11-14 14:23:20 +0700

    [EDIT] Dashboard for responsive all size

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,15 +1,15 @@
-import React, { Component , Fragment } from 'react';
-import { GET_LIST, GET_MANY, Responsive, Title } from 'react-admin';
+import React, { Component , Fragment } from 'react'
+import { GET_LIST, GET_MANY, Responsive, Title } from 'react-admin'
 import { Link } from 'react-router-dom'
-import TimetableProcess from './TimetableProcess';
-import Timetables from './Timetables';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { Grid, Button, Paper, Typography, Card, CardContent, CircularProgress} from '@material-ui/core';
+import TimetableProcess from './TimetableProcess'
+import Timetables from './Timetables'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import { Grid, Button, Paper, Typography, Card, CardContent, CircularProgress} from '@material-ui/core'
 import Subject from './Subject'
 import Section from './Section'
 import AddIcon from '@material-ui/icons/Add'
-import axios from 'axios';
+import axios from 'axios'
 import loopbackRestClient from 'aor-loopback'
 
 const dataProvider = loopbackRestClient(process.env.REACT_APP_API_ENDPOINT)
@@ -43,7 +43,7 @@ const style = theme => ({
     
 });
 const styles = {
-  flex: { display: 'flex' },
+  flex: { display: 'flex' , marginLeft:'1em'},
   flexColumn: { display: 'flex', flexDirection: 'column' },
   leftCol: { flex: 1, marginRight: '1em' },
   rightCol: { flex: 1, marginLeft: '1em' },
@@ -86,37 +86,41 @@ class Dashboard extends React.Component {
                     xsmall={
                         <div>
                             <div style={styles.flexColumn}>
-                                <div style={{ marginBottom: '2em', marginRight: '1em' }}>
-                                <Button variant="contained" color="primary" className={classes.card} style={{ float :'right' }} component={Link} to={`/subjectSections`}>
+                                <Button variant="contained" color="primary" className={classes.button} style={{ float :'right', padding: 20, margin: '1em' }} component={Link} to={`/subjectSections`}>
                                      <AddIcon />  
                                      Create timetable
                                     </Button>
-                                </div>
-                                <div style={styles.flex}>
-                                
-                                <TimetableProcess />
-                                
-                                </div>
-                                <div style={styles.singleCol}>
-                                   
-                                </div>
+                                    <TimetableProcess />
+                                    {this.state.list.map(term => 
+                                    <Card className={classes.card} style={{ margin: '1em' }} >
+                                        <Typography variant='headline' component='h1'  button component={Link} to={'/timetables/undefined/show'}>
+                                          {term}
+                                        </Typography>
+                                    </Card>
+                                     ) }
                             </div>
                         </div>
                     }
                     small={
+                      <div>
                         <div style={styles.flexColumn}>
-                            <div style={styles.singleCol}>
-                            <Button variant="contained" color="primary" className={classes.button} style={{ float :'right'}} component={Link} to={`/subjectSections`}>
-                                     <AddIcon />  
-                                     Create timetable
-                                    </Button>
-                            </div>
-                            <div style={styles.flex}>
-                              <TimetableProcess />
-                            </div>
-                            <div style={styles.singleCol}>
-                            </div>
+                        <div style={styles.rightCol}>
+                        <Button variant="contained" color="primary" className={classes.button} style={{ float :'right', margin: '1em' }} component={Link} to={`/subjectSections`}>
+                               <AddIcon />  
+                               Create timetable
+                              </Button>
                         </div>
+                          
+                              <TimetableProcess />
+                              {this.state.list.map(term => 
+                                <Card className={classes.card} style={{ margin: '1em' }} >
+                                    <Typography variant='headline' component='h1' button component={Link} to={'/timetables/undefined/show'}>
+                                      {term}
+                                    </Typography>
+                                </Card>
+                               ) }
+                        </div>
+                      </div>
                     }
                     medium={
                       <div className={classes.root}>
@@ -133,7 +137,7 @@ class Dashboard extends React.Component {
                         {this.state.list.map(term => 
                         <Grid item md={6}>
                         <div className={classes.main}>
-                          <Card className={classes.card} style={{ marginLeft: 10 }} >
+                          <Card className={classes.card}  >
                               <Typography variant='headline' component='h1'  button component={Link} to={'/timetables/undefined/show'}>
                                 {term}
                               </Typography>
@@ -143,70 +147,11 @@ class Dashboard extends React.Component {
 
                       </Grid>
                     </div>
-                      // <div>
-                      //   <Button variant="contained" color="primary" className={classes.button} style={{ float :'right'}} component={Link} to={`/subjectSections`}>
-                      //     <AddIcon />  
-                      //     Create timetable
-                      //   </Button>
-                      //   <div style={styles.flex}>
-                         
-                      //       <div style={styles.leftCol}>
-                      //           <div style={styles.flex} >
-                      //           {/* <Button variant="contained" color="primary" className={classes.main} style={{ float :'right', backgroundColor: 'white' , color:'black'}} component={Link} to={`/subjectSections`}>
-                      //                <AddIcon />  
-                      //                Create timetable
-                      //               </Button>
-                      //               <Subject /> */}
-                      //           <TimetableProcess />
-                      //           </div>
-                      //           <div style={styles.singleCol}>
-                                
-                      //           </div>
-                      //           <div style={styles.singleCol}>
-                      //           </div>
-                      //       </div>
-                      //       <div style={styles.rightCol}>                               
 
-                      //           <div style={styles.flex}> 
-                      //           <Timetables />
-                                
-                      //           {/* <Section />
-                      //           <Subject /> */}
-                      //           </div>
-                      //       </div>
-                      //   </div>
-                      //   </div>
                     }
                 />
             </Fragment>
 
-      // <Grid container className={classes.root} spacing={24}>
-      //   <Grid item xs={12}>
-      //   <Grid container className={classes.demo} justify='flex-end' spacing={Number(spacing)}>
-      //     <Button variant="contained" color="primary" className={classes.button}  component={Link} to={`/subjectSections`}>
-      //        <AddIcon />  Create timetable
-      //     </Button>
-      //     </Grid>
-      //   </Grid>
-      //   <Grid item xs={6}>
-      //     <Grid container className={classes.demo} justify='flex-start' spacing={Number(spacing)}>
-      //       <Grid item>
-      //         <Card className={classes.graph} style={{ marginLeft: 20 }} >
-      //           <CardContent>
-      //             <Grid container className={classes.demo} justify='flex-start' spacing={Number(spacing)}>
-      //               <TimetableProcess />
-      //             </Grid>
-      //           </CardContent>
-      //         </Card>
-      //       </Grid>
-      //     </Grid> 
-      //   </Grid>
-      //   <Grid item xs={6}>
-      //     <Grid container className={classes.demo} justify='flex-start' spacing={Number(spacing)}>
-      //       <Timetables />
-      //     </Grid>
-      //   </Grid>
-      // </Grid>
     );
   }
 }
