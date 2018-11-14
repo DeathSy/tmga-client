449dfffaeabdaff1a3c734f45a65edc1143d9787
Miiwn <miiwnpmawt@gmail.com>
2018-11-14 13:20:19 +0700

    [EDIT] delete student create gen/lng and contrains page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { FixedSubjectList, FixedSubjectEdit, FixedSubjectCreate, FixedSubjectIco
 import { ConstrainList, ConstrainEdit, ConstrainCreate, ConstrainIcon } from './Constrains.js'
 import { SectionList, SectionEdit, SectionCreate, SectionIcon } from './Sections.js'
 import { SubjectFormatList, SubjectFormatEdit, SubjectFormatCreate, SubjectFormatIcon } from './SubjectFormats.js'
-import { StudentList, StudentEdit, StudentCreate, StudentIcon } from './Students.js'
 import { TimetableList, TimetableShow, TimetableIcon } from './Timetable'
 // import { CreateView, CreateIcon } from './CreateTimetable'
 import Dashboard from './dashboard/Dashboard'
@@ -23,16 +22,15 @@ class App extends Component {
         theme={tmgaTheme}
         title={'TMGA'}
         dataProvider={dataProvider} >
-        <Resource name='subjectSections' options={{ label: 'Create timetable' }} list={SectionList} edit={SectionEdit} create={SectionCreate} icon={SectionIcon} />
+        {/* <Resource name='subjectSections' options={{ label: 'Create timetable' }} list={SectionList} edit={SectionEdit} create={SectionCreate} icon={SectionIcon} /> */}
         <Resource name='timetablesview' />
         <Resource name='timetables' options={{ label: 'Timetable' }} show={TimetableShow} list={TimetableList} icon={TimetableIcon} />
         <Resource name='rooms' list={RoomList} edit={RoomEdit} create={RoomCreate} icon={RoomIcon} />
         <Resource name='lecturers' list={LecturerList} edit={LecturerEdit} create={LecturerCreate} icon={LecturerIcon} />
         <Resource name='subjects' list={SubjectList} edit={SubjectEdit} create={SubjectCreate} icon={SubjectIcon} />
-        <Resource name='fixedSubjects' options={{ label: 'GEN & LNG' }} list={FixedSubjectList} edit={FixedSubjectEdit} create={FixedSubjectCreate} icon={FixedSubjectIcon} />
-        <Resource name='constrains' options={{ label: 'Constraints' }} list={ConstrainList} edit={ConstrainEdit} create={ConstrainCreate} icon={ConstrainIcon} />
+        {/* <Resource name='fixedSubjects' options={{ label: 'GEN & LNG' }} list={FixedSubjectList} edit={FixedSubjectEdit} create={FixedSubjectCreate} icon={FixedSubjectIcon} />
+        <Resource name='constrains' options={{ label: 'Constraints' }} list={ConstrainList} edit={ConstrainEdit} create={ConstrainCreate} icon={ConstrainIcon} /> */}
         <Resource name='subjectFormats' options={{ label: 'Subject Type' }} list={SubjectFormatList} edit={SubjectFormatEdit} create={SubjectFormatCreate} icon={SubjectFormatIcon} />
-        <Resource name='students' list={StudentList} edit={StudentEdit} create={StudentCreate} icon={StudentIcon} />
         <Resource name='timeSlots' />
       </Admin>
     )
